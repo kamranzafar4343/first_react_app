82c0cb339bb5e21a9b723879137a0491ee55bc46
Kamran Zafar <kamranzafar4343@gmail.com>
2026-06-23 12:29:52 +0500

    upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// 6ea75112
+
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=6ea75112';
+
+const App = () => {
+  return ( 
+    <h1>App</h1>
+    );
 }
 
 export default App;
